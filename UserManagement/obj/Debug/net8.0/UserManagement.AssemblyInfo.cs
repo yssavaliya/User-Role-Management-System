aeaf74e1408e7a242effeb950189c3c491c342f2
yssavaliya <ysavaliya215@rku.ac.in>
2024-06-15 19:51:50 +0530

    Initial commit3

--- UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
+++ UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33de13752022d12333141029c0997ea7059f0aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
